f4d3d1f32259c8d961e352684cba86956e9c611a
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-07 11:42:15 -0500

    Save button working, saving user state to database

--- scripts/urbanDweller.js
+++ scripts/urbanDweller.js
@@ -1,4 +1,7 @@
+import { setSocioLocationId } from "./transientState.js";
+
 export const locationTypeChoices = async () => {
+  document.addEventListener("change", handleLocationChange);
   const response = await fetch("http://localhost:8088/socioLocations");
   const locations = await response.json();
   let html = `<h2> Which type of area do you live in?</h2>`;
@@ -7,4 +10,11 @@ export const locationTypeChoices = async () => {
     html += `<input type="radio" name="location" value="${location.id}" /> ${location.label}`;
   }
   return html;
+};
+
+const handleLocationChange = (changeEvent) => {
+  if (changeEvent.target.name === "location") {
+    const converted = parseInt(changeEvent.target.value);
+    setSocioLocationId(converted);
+  }
 };
