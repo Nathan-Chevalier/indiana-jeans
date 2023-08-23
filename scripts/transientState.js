a6b1170a45ac5ed77b6451e728e0f9ac39a1f96c
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-23 14:21:00 -0500

    Map method implemented to transient state

--- scripts/transientState.js
+++ scripts/transientState.js
@@ -1,27 +1,28 @@
 // Set up the transient state data structure and provide initial values
-const transientState = {
-  ownsBlueJeans: false,
-  socioLocationId: 0,
-};
+const transientState = new Map();
+transientState.set("ownsBlueJeans", false);
+transientState.set("socioLocationId", 0);
+// ownsBlueJeans: false,
+// socioLocationId: 0,
+
 // Functions to modify each property of transient state
 export const setOwnsBlueJeans = (chosenOwnership) => {
-  transientState.ownsBlueJeans = chosenOwnership;
-  console.log(transientState);
+  transientState.set("ownsBlueJeans", chosenOwnership);
 };
 
 export const setSocioLocationId = (chosenLocation) => {
-  transientState.socioLocationId = chosenLocation;
-  console.log(transientState);
+  transientState.set("socioLocationId", chosenLocation);
 };
 
 // Function to convert transient state to permanent state
 export const saveSurveySubmission = async () => {
+  const objectTrans = Object.fromEntries(transientState);
   const postOptions = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(transientState),
+    body: JSON.stringify(objectTrans),
   };
 
   const response = await fetch(
